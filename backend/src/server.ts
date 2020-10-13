e2f3848d2470c1fe8edba8042324e27b1d7dcb93
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2020-10-13 13:26:30 -0300

    added cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import "express-async-errors";
 import "./database/connection";
 import errorHandler from "./errors/handler";
@@ -10,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
-
+app.use(cors());
 app.use(errorHandler);
 
 app.listen(3333);
